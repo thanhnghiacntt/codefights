c2a9bb4cc68cb46f07d32363f5f307016ea377e3
Nguyễn Thành Nghĩa <nghiant@dekiconnect.com>
2018-03-28 15:08:39 +0700

    Update source

--- java/findSubstrings.java
+++ java/findSubstrings.java
@@ -3,48 +3,25 @@ You have two arrays of strings, words and parts. Return an array that contains t
 
 If several parts substrings occur in one string in words, choose the longest one. If there is still more than one such part, then choose the one that appears first in the string.
 */
-String[] findSubstrings(String[] words, String[] parts) {
-    HashMap<String, Boolean> hash = new HashMap<String, Boolean>();
-    String[] newParts = new String[parts.length];
-    for (String string : parts) {
-        hash.put(string, true);
-    }
-    int count = 0;
-    for (int i = 5; i > 0; i--) {
-        for (String part : parts) {
-            if (part.length() == i && hash.containsKey(part)) {
-                newParts[count] = part;
-                count++;
-            }
-        }
-    }
 
-    for (int i = 0; i < words.length; i++) {
-        String word = words[i];
-        int position = 100;
-        String match = "";
-        int length = -1;
-        for (String part : newParts) {
-            if (length > 0 && part.length() != length) {
-                break;
-            }
-            int index = word.indexOf(part);
-            if(index == -1){
-                continue;
-            }
-            if (index < position && part.length() == length) {
-                position = index;
-                match = part;
-            }
-            if (length == -1) {
-                position = index;
-                length = part.length();
-                match = part;
-            }
-        }
-        if (length != -1) {
-            words[i] = word.replaceFirst(match, "[" + match + "]");
-        }
-    }
-    return words;
+String[] findSubstrings(String[] words, String[] parts) {
+	HashMap<String, Boolean> hash = new HashMap<String, Boolean>();
+	for (String string : parts) {
+		hash.put(string, true);
+	}
+	for (int i = 0; i < words.length; i++) {
+		String word = words[i];
+		int len = word.length();
+		for (int j = 5; j > 0; j--) {
+			for (int k = 0; k <= len - j; k++) {
+				String sub = word.substring(k, k + j);
+				if (hash.containsKey(sub)) {
+					words[i] = word.replaceFirst(sub, "[" + sub + "]");
+					j = -1;// Break loop out
+					break;
+				}
+			}
+		}
+	}
+	return words;
 }
